16dc21eb5019eaa3251e0773eae15963640584a3
Daniel Oliva <kalgcny@gmail.com>
2018-01-09 20:21:57 -0500

    Fragments

--- FragmentsExamples/app/src/main/java/com/danieleoliva/fragmentsexamples/ListFrag.java
+++ FragmentsExamples/app/src/main/java/com/danieleoliva/fragmentsexamples/ListFrag.java
@@ -1,29 +1,64 @@
 package com.danieleoliva.fragmentsexamples;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ListFrag extends Fragment {
+public class ListFrag extends ListFragment {
 
+    private ChessPieceListener chessPieceListener;
 
     public ListFrag() {
         // Required empty public constructor
-    }
 
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.list_frag, container, false);
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        setListAdapter(new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.pieces)));
+
+        chessPieceListener.onChessPieceSelected(0);
+
     }
 
+    public interface ChessPieceListener {
+        public void onChessPieceSelected(int index);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            chessPieceListener = (ChessPieceListener) context;
+        }
+        catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement the interface called " + " ChessPieceListener!");
+        }
+
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+
+        chessPieceListener.onChessPieceSelected(position);
+
+    }
 }
--- FragmentsExamples/app/src/main/java/com/danieleoliva/fragmentsexamples/MainActivity.java
+++ FragmentsExamples/app/src/main/java/com/danieleoliva/fragmentsexamples/MainActivity.java
@@ -2,12 +2,29 @@ package com.danieleoliva.fragmentsexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements ListFrag.ChessPieceListener {
+
+    TextView tvDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        tvDetails = (TextView) findViewById(R.id.tvDetails);
+
+    }
+
+    @Override
+    public void onChessPieceSelected(int index) {
+
+        String [] descriptions = getResources().getStringArray(R.array.descriptions);
+
+        tvDetails.setText(descriptions[index]);
+
     }
 }
