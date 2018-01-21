e84fb815a29bf23d16d758c621961f4812886b4e
Daniel Oliva <kalgcny@gmail.com>
2018-01-20 21:57:53 -0500

    learend more google maps features

--- IntroMapsVideo/app/src/main/java/com/danieleoliva/intromapsvideo/MapsActivity.java
+++ IntroMapsVideo/app/src/main/java/com/danieleoliva/intromapsvideo/MapsActivity.java
@@ -11,12 +11,17 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static final LatLng VIRGINIA_HIGHLANDS = new LatLng(33.782381, -84.354334);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +47,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        mMap.addMarker(new MarkerOptions()
+//                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.house))
+//                .anchor(0.0f, 1.0f)
+//                .title("Virginia Highlands")
+//                .position(VIRGINIA_HIGHLANDS));
+
+
     }
 
     @Override
